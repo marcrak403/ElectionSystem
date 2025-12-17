84c0cd6186b721e20e4ddc97893397e1edeca6e3
Marcin Rak <Marcin@10.252.0.3>
2025-12-17 13:13:31 +0100

    Initialize Spring Boot application

---
+++ src/main/java/com/rakit/electionsystem/ElectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rakit.electionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionSystemApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ElectionSystemApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/rakit/electionsystem/ElectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakit.electionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionSystemApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
